00eda2d50210bb20fd75bed76995932c961447c0
nemanjapetrasinovic <nemanjapet94@gmail.com>
2017-08-14 23:24:36 +0200

    dugme za open main activity

--- app/src/main/java/com/example/nemanja/upoznajgrad/LoginActivity.java
+++ app/src/main/java/com/example/nemanja/upoznajgrad/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.nemanja.upoznajgrad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button map=(Button) findViewById(R.id.button2);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openMain=new Intent(LoginActivity.this.getApplicationContext(),MainActivity.class);
+                startActivity(openMain);
+            }
+        });
     }
 }
