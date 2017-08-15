9a9ce22dc7f0fa210a3ba5f0cc9b035b8893fcdf
markopesterac <markopesterac@elfak.rs>
2017-08-15 22:06:33 +0200

    fix

--- app/src/main/java/com/example/nemanja/upoznajgrad/LoginActivity.java
+++ app/src/main/java/com/example/nemanja/upoznajgrad/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.nemanja.upoznajgrad;
 
 import android.content.Intent;
+
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,12 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -105,5 +112,15 @@ public class LoginActivity extends AppCompatActivity {
                         // ...
                     }
                 });
+
+        Button map=(Button) findViewById(R.id.button2);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openMain=new Intent(LoginActivity.this.getApplicationContext(),MainActivity.class);
+                startActivity(openMain);
+            }
+        });
+
     }
 }
