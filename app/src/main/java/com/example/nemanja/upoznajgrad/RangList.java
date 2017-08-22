889a4bdf564a7cef6a614c17715c4391e53f7499
marijatrajkovic <marija94trajkovic@live.com>
2017-08-22 22:38:33 +0200

    Rang

--- app/src/main/java/com/example/nemanja/upoznajgrad/RangList.java
+++ app/src/main/java/com/example/nemanja/upoznajgrad/RangList.java
@@ -6,28 +6,71 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static com.example.nemanja.upoznajgrad.R.*;
 
 public class RangList extends AppCompatActivity {
+
+    DatabaseReference dref;
+    ListView listview;
+    ArrayList<String> list=new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rang_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(layout.activity_rang_list);
+        Toolbar toolbar = (Toolbar) findViewById(id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        listview=(ListView)findViewById(id.listview);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_activated_1,list);
+        listview.setAdapter(adapter);
+        dref=FirebaseDatabase.getInstance().getReference("user");
+        dref.orderByChild("score").addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Korisnik korisnik = dataSnapshot.getValue(Korisnik.class);
+                String value= korisnik.getFirstname()+" "+korisnik.getLastname() +" "+korisnik.getScore();
+                list.add(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Korisnik korisnik = dataSnapshot.getValue(Korisnik.class);
+                String value= korisnik.getFirstname()+" "+korisnik.getLastname() +" "+korisnik.getScore();
+                list.remove(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
-
-
-
     }
 
 }
