456bd980d89f8b48af5bc59ed248994249589b26
marijatrajkovic <marija94trajkovic@live.com>
2017-08-24 15:49:31 +0200

    1111111111

--- app/src/main/java/com/example/nemanja/upoznajgrad/QuizResultsActivity.java
+++ app/src/main/java/com/example/nemanja/upoznajgrad/QuizResultsActivity.java
@@ -2,12 +2,24 @@ package com.example.nemanja.upoznajgrad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class QuizResultsActivity extends AppCompatActivity {
+
+    TextView tacni,netacni;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_results);
+
+        String correct = getIntent().getStringExtra("brojTacnih");
+        String wrong= String.valueOf(4-Integer.parseInt(correct));
+
+        tacni=(TextView) findViewById(R.id.tacni);
+        tacni.setText(correct);
+
+        netacni=(TextView) findViewById(R.id.netacni);
+        netacni.setText(wrong);
     }
 }
